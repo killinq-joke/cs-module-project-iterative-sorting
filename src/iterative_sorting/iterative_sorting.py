2ba418cd897b416f328fbb71a8e39761548a104a
zakaria touzri <zakadmaster@gmail.com>
2020-07-20 20:52:46 +0200

    ADDED: count array for counting_dort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -56,4 +56,11 @@ What is the time and space complexity of the counting sort algorithm?
 def counting_sort(arr, maximum=None):
     # Your code here
 
-    return arr
+    count = [0 for i in range(0, maximum + 1)]
+
+
+    return count
+
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(counting_sort(arr1, 10))
