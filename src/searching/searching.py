a73b2131833b4f9f47a47fcd771034eae1d50936
zakaria touzri <zakadmaster@gmail.com>
2020-07-20 22:14:58 +0200

    ADDED: basic binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,3 +1,6 @@
+import math
+
+
 def linear_search(arr, target):
     # Your code here
     for i in range(0, len(arr)):
@@ -11,5 +14,18 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    mid = math.ceil(len(arr) / 2)
+    print(arr[mid])
+    while arr[mid] != target:
+        if arr[mid] > target:
 
-    return -1  # not found
+            mid = math.ceil(mid / 2)
+            print(mid)
+        else:
+            mid += math.ceil(mid / 2)
+
+    return mid or -1  # not found
+
+
+arr1 = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
+print(binary_search(arr1, -4))
